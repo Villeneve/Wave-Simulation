e299eb9f23066743fa2ee9c9caf3665fe5bd0ffa
Villeneve <villeneveoliveira@gmail.com>
2024-11-02 11:10:10 -0300

    Alterações de Emanuel

--- ondas.py
+++ ondas.py
@@ -13,16 +13,6 @@ c = 1
 t = np.arange(0,.2,0.000000001)
 print(t.shape)
 
-#z = np.zeros((100))
-# print(z.shape)
-# for i in range(100):
-#     for j in range(100):
-#         r = np.sqrt(np.power(i,2)+np.power(j,2))
-#         if r != 0: z[i,j] = 1/r*np.sin((w/c)*(r)-w*t)
-
-#r = np.sqrt(np.power(X,2)+np.power(Y,2))
-#z = 1/r*np.sin((w/c)*(r)-w*t)
-
 plt.ion()
 r = np.sqrt(np.power(X,2)+np.power(Y,2))
 z = 1/r*np.sin((w/c)*(r)-w*1)
